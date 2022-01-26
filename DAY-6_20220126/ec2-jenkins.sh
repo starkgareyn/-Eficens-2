84c2ef4426da7f208d5ba9ad9bf0f971398e9491
kesavkummari <kesav.kummari@gmail.com>
2022-01-26 22:57:53 +0530

    Jenkins, Jfrog and Sonarqube

---
+++ DAY-6_20220126/ec2-jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# Jenkins Server on Ubuntu 20.04
+aws ec2 run-instances \
+--image-id "ami-01f87c43e618bf8f0" \
+--instance-type "t2.medium" \
+--count 1 \
+--subnet-id "subnet-fa64cca0" \
+--security-group-ids "sg-0ab38acb31986250b" \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Jenkins},{Key=Environment,Value=Development}]' \
+--key-name "aws_california" \
+--user-data file://install-jenkins.txt --profile devops 
