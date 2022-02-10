253e1810e1e06c5d534d6940a79ea8f4746ec3d8
kesavkummari <kesav.kummari@gmail.com>
2022-02-10 23:02:00 +0530

    k8s cluster

---
+++ DAY-15_20220210/ec2-k8s-controller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create a Linux Operating System using AWS CLI Commands
+aws ec2 run-instances \
+--image-id "ami-0851b76e8b1bce90b" \
+--instance-type "c5.xlarge" \
+--count 1 \
+--subnet-id "subnet-421fef29" \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=k8s-controller-plane},{Key=Environment,Value=Development},{Key=Project Name,Value=Hatigen},{Key=Project ID,Value=20211204},{Key=Email ID,Value=trainings@hatigen.com},{Key=Mobile Number,Value=+91}]'  \
+--security-group-ids  "sg-06e4607030e3448cf"   \
+--key-name "softobiz_keys" \
+--user-data file://k8s-controller-install.txt --profile devops
